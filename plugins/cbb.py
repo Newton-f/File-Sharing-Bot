e4f50049e85a3dfab685b3dc04e8ea25f42c2f50
Rinne <xyzyo983@gmail.com>
2025-02-17 20:25:45 +0545

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>◈ ᴍʏ ɴᴀᴍᴇ :</b> <a href='https://t.me/mi_ku_pie_bot'>ᴍɪᴋᴀ*</a> \n<b>◈ ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ :</b> <a href='https://t.me/B_ani_me'>ᴀɴɪᴍᴇ ᴄʀᴜɪsᴇ</a> \n<b>◈ ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ :</b> <a href='https://t.me/b_movies_allin1'>ᴍᴏᴠɪᴇ ᴄʀᴜɪsᴇ</a> \n<b>◈ ᴅʀᴀᴍᴀ ᴄʜᴀɴɴᴇʟ :</b> <a href='https://t.me/kdrama_in_hindihd'>ᴅʀᴀᴍᴀ ᴄʀᴜɪsᴇ</a> \n<b>◈ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ :</b> <a href='https://t.me/backup_forall'>ʙᴀᴄᴋ ᴜᴘ</a> \n<b>◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ :</b> <a href='https://t.me/ad_minn_bot'>ᴀᴅᴍɪɴ</a>",
+            text = f"<b>◈ ᴍʏ ɴᴀᴍᴇ :</b> <a href='https://t.me/mi_ku_pie_bot'>ᴍɪᴋᴀ*</a> \n<b>◈ ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ :</b> <a href='https://t.me/full_backu/10'>ᴀɴɪᴍᴇ ᴄʀᴜɪsᴇ</a> \n<b>◈ ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ :</b> <a href='https://t.me/full_backu/10'>ᴍᴏᴠɪᴇ ᴄʀᴜɪsᴇ</a> \n<b>◈ ᴅʀᴀᴍᴀ ᴄʜᴀɴɴᴇʟ :</b> <a href='https://t.me/full_backu/10'>ᴅʀᴀᴍᴀ ᴄʀᴜɪsᴇ</a> \n<b>◈ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ :</b> <a href='https://t.me/full_backu'>ʙᴀᴄᴋ ᴜᴘ</a> \n<b>◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ :</b> <a href='https://t.me/ad_minn_bot'>ᴀᴅᴍɪɴ</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
